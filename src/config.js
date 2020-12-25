43c27ae884a9519c354244544c2c038f9540ca00
Narmdo <narmdo@gmail.com>
2020-12-25 16:16:50 +1100

    Fixed redirect

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export default {
     "endpoint" : "local",
     "client_id": "client_93258882-27bf-4dde-9efd-c7c8b7a14d78",
     "scope": "openid friend.add friend.view",
-    "redirect_uri": "https://accounts.townshiptale.com/demos/att-friend-manager/logged-in"
+    "redirect_uri": "https://alta-vr.github.io/Demos-ATT-Friend-Manager/logged-in"
 }
